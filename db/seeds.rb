1e57b14b93d89fcd0f3317fd51ddc26b33478d0c
Vivek Patel <vnp229@gmail.com>
2016-04-20 08:10:56 -0500

    added seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,16 @@
 
 User.create(user_name:"Vivek", password:"password")
 Cheese.create(name:"Cheddar", user_id: 1)
+
+99.times do
+  User.create(user_name:Faker::Internet.user_name, password:"password")
+end
+
+cheeses = ["Cheddar", "Gouda", "Brie", "Pepper Jack", "Meunster", "Provolone", "Swiss", "Parmesan", "Gruyere", "Gorgonzola"]
+10.times do
+  Cheese.create(name:cheeses.pop, description: Faker::Company.bs, user_id: rand(1..5))
+end
+
+25.times do
+  Favorite.create(user_id: rand(1..99), cheese_id:rand(1..10))
+end
